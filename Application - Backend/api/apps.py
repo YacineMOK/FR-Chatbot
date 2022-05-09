bd5ddc2ed4c15f72d936e59761f51f51a8574fc4
Yacine Mokhtari <yacine_mokhtari@outlook.fr>
2022-05-09 16:39:01 +0200

    corrected the model's name

--- Application - Backend/api/apps.py
+++ Application - Backend/api/apps.py
@@ -6,5 +6,5 @@ from django.conf import settings
 
 class ApiConfig(AppConfig):
     name = 'api'
-    MODEL_FILE = os.path.join(settings.MODELS, "WhyNot.joblib")
+    MODEL_FILE = os.path.join(settings.MODELS, "SimpleT5.joblib")
     model = joblib.load(MODEL_FILE)
